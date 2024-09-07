6b4ac94f79026bd40debeefdd9d3972c65c29a05
Taldariner <71024372+Taldariner@users.noreply.github.com>
2024-09-07 20:43:41 +0300

    [Godot] Updated map to GridMap and fixed portal crash on multiple objects transition
    Updated map to GridMap from MultiMeshInstance3D, as it looks more suitable for my current needs and allows to configure map visually tight from editor, instead of using script.
    Also updated portals script, and now it seems not to crash when many objects are transmitted through it at the same time.
    Unfortunately, I started getting visual glitches when transferring through portal again for some reason, maybe because of updated portal script. Will need to investigate it later, find the reason and fix it, of course, if I can, I suppose.

--- Godot/scenes/portal.gd
+++ Godot/scenes/portal.gd
@@ -27,7 +27,6 @@ func update_everything():
 	self_viewport.size = player_camera.get_viewport().get_visible_rect().size
 	self_viewport.msaa_3d = player_camera.get_viewport().msaa_3d
 	self_viewport.screen_space_aa = player_camera.get_viewport().screen_space_aa
-	# self_viewport.use_taa = player_camera.get_viewport().use_taa
 	self_viewport.use_debanding = player_camera.get_viewport().use_debanding
 	self_viewport.use_occlusion_culling = player_camera.get_viewport().use_occlusion_culling
 	self_viewport.mesh_lod_threshold = player_camera.get_viewport().mesh_lod_threshold
@@ -41,39 +40,34 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		var local_pos = global_transform.affine_inverse() * body.global_transform.origin
 		var previous_object_z = _nonzero_sign(local_pos.z)
 		var current_object_z = _nonzero_sign(local_pos.z)
-		print("Body entered: ", body.name)
+		# print("Body entered: ", body.name)
 		tracked_objects.append([body, previous_object_z, current_object_z])
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	for object_index in tracked_objects.size():
-		if tracked_objects[object_index][0].name == body.name:
-			print("Body exited: ", body.name)
-			tracked_objects.remove_at(object_index)
+	for object in tracked_objects:
+		if object[0] == body:
+			tracked_objects.erase(object)
 
 func move_to_other_portal(object: Array):
 	var body = object[0]
 	var offset = body.global_transform.origin - global_transform.origin
-	var camera_offset = player_camera.global_transform.origin - global_transform.origin
-	self_camera.global_transform.origin = connected_portal.global_transform.origin + (connected_portal.global_transform.origin + camera_offset - global_transform.origin)
-	self_camera.global_transform.basis = player_camera.global_transform.basis
+	if body.name == "CharacterBody3D":
+		var camera_offset = player_camera.global_transform.origin - global_transform.origin
+		self_camera.global_transform.origin = connected_portal.global_transform.origin + (connected_portal.global_transform.origin + camera_offset - global_transform.origin)
+		self_camera.global_transform.basis = player_camera.global_transform.basis
 	body.global_transform.origin = connected_portal.global_transform.origin + offset
 	
-	for object_index in tracked_objects.size():
-		if tracked_objects[object_index][0].name == body.name:
-			tracked_objects.remove_at(object_index)
-	for object_index in connected_portal.tracked_objects.size():
-		if connected_portal.tracked_objects[object_index][0].name == body.name:
-			connected_portal.tracked_objects.remove_at(object_index)
+	tracked_objects.erase(object)
+	connected_portal.tracked_objects.erase(object)
+	# print("Object teleported: ", object)
 
 func track_objects_pass():
-	for object_index in tracked_objects.size():
-		var object = tracked_objects[object_index]
+	for object in tracked_objects:
 		var local_pos = global_transform.affine_inverse() * object[0].global_transform.origin
-		print(object[0].name)
 		object[2] = _nonzero_sign(local_pos.z)
 		if object[1] != object[2]:
-			print(object[1], "<->", object[2])
-			print(object[0].name, ": Object has crossed the plane!")
+			# print(object[1], "<->", object[2])
+			# print(object[0].name, ": Object has crossed the plane!")
 			move_to_other_portal(object)
 		object[1] = object[2]
 
